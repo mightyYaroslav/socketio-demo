fe73bcc81df045739080c14d5b4e70fb2d359826
mightyYaroslav <yaroslavsmth@gmail.com>
2021-04-27 19:54:59 +0300

    fix the demo with styles

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const {Server} = require('socket.io')
 const io = new Server(server)
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/test.html')
 })
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000')
-})
+  console.log(`Socket.IO server running at http://localhost:${3000}/`);
+});
